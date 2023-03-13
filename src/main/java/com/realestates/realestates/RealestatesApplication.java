31cabb21635451330de71423277229a7e01e8b57
Blairi <axel.avi.fer@gmail.com>
2023-03-13 15:34:30 -0600

    first commit

---
+++ src/main/java/com/realestates/realestates/RealestatesApplication.java
@@ -0,0 +1,13 @@
+package com.realestates.realestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestatesApplication.class, args);
+	}
+
+}
