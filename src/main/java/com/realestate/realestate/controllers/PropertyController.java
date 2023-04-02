be402dd87a6925d630c60d33f9fbed58b0f164f0
Blairi <axel.avi.fer@gmail.com>
2023-04-01 19:08:34 -0600

    request method POST add-property

--- src/main/java/com/realestate/realestate/controllers/PropertyController.java
+++ src/main/java/com/realestate/realestate/controllers/PropertyController.java
@@ -1,13 +1,28 @@
 package com.realestate.realestate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.realestate.realestate.domain.Property;
+import com.realestate.realestate.service.PropertyService;
 
 @Controller
 public class PropertyController {
 	
-	@RequestMapping("add-property")
+	@Autowired
+	private PropertyService service;
+	
+	@RequestMapping(value="add-property", method = RequestMethod.GET)
 	public String showNewPropertyPage() {
 		return "admin/property";
+	}
+	
+	@RequestMapping(value="add-property", method = RequestMethod.POST)
+	public String addNewProperty(ModelMap model, Property property) {
+		service.addProperty(property);
+		return "redirect:dashboard";
 	}
 }
